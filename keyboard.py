ebc425d71e60dc8ad1967a29ea0a5420038af4c3
Mr. Talsorian <wotgamer111007@gmail.com>
2023-10-03 08:31:50 -0700

    Com

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,5 @@
 from aiogram import types
 
-buttons = [[types.InlineKeyboardButton(text='Open Lotery', web_app=types.WebAppInfo(url='index.html'))]]
+buttons = [[types.InlineKeyboardButton(text='Open Lotery', web_app=types.WebAppInfo(url='https://github.com/JavaBestFullCoder/TelegramLoteryBot/blob/ddcb74986b8bf2db3e18b31fd90de20d00563091/index.html'))]]
 
 markup = types.InlineKeyboardMarkup(inline_keyboard=buttons)
