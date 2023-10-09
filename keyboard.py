d4fedae60fcfebc541d51758415773aeefa2705d
Mani <108091806+JavaBestFullCoder@users.noreply.github.com>
2023-10-09 08:17:48 -0700

    Delete keyboard.py

--- keyboard.py
+++
@@ -1,5 +0,0 @@
-from aiogram import types
-
-buttons = [[types.InlineKeyboardButton(text='Open Lotery', web_app=types.WebAppInfo(url='https://github.com/c418f853-b820-46a7-b9cc-df72bb98c4b6'))]]
-
-markup = types.InlineKeyboardMarkup(inline_keyboard=buttons)
